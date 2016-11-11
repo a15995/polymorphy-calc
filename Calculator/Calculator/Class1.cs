fe38f923d6cc0972ff8bb06af8e1e70ccf0aca09
a15995 <123@123.dk>
2016-11-11 14:38:55 +0100

    Calc vers 2

---
+++ Calculator/Calculator/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public abstract class Expression
+    {
+        public abstract double Evaluate();
+    }
+
+    public class ConstantExpression : Expression
+    {
+        private readonly double value
+        {
+            get { return value; }
+        }
+
+        public override ToString()
+        {
+            return base.ToString();)
+        }
+        
+    }
+
+    sealed override Evaluate()
+    {
+        base.ToString();
+    }
+   
+
+    public abstract class BinaryExpression : Expression
+    {
+        protected Expression left;
+        protected Expression right;
+
+        abstract readonly string OperatorSymbol;
+
+    }
+
+ }
