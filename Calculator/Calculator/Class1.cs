185aa8efdd8ded7fc142787e77e44b436129608b
SL <SL>
2016-11-13 11:45:10 +0100

    Søndag 11:45

--- Calculator/Calculator/Class1.cs
+++ Calculator/Calculator/Class1.cs
@@ -6,38 +6,73 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    public abstract class Expression
+    public abstract class Expression // 1. Lav en abstrakt klasse Expression:
     {
-        public abstract double Evaluate();
+        public abstract double Evaluate(); // a. med en abstrakt metode Evaluate uden parametre, der returnerer en double.
     }
 
-    public class ConstantExpression : Expression
+    public sealed class ConstantExpression : Expression // 2. Lav Expression underklasse ConstantExpression: (d. Skal ikke kunne nedarves fra.)
     {
-        private readonly double value
+        private readonly double value; // a. Med privat readonly felt kaldet value af typen double.
+
+        public new double Evaluate // b. Med implementation af Evaluate,
         {
-            get { return value; }
+            get { return value; } // der returnerer value.
         }
 
-        public override ToString()
+        public override string ToString // c. Med overstyring af ToString,
         {
-            return base.ToString();)
-        }
+            get { return value.ToString(); } //  der udskriver value.
+        }   
+    }
+
+    public abstract class BinaryExpression : Expression // 3. Lav abstrakt Expression underklasse BinaryExpression:
+    {
+        protected Expression left; // a. Med to felter left
+        protected Expression right; // og right af typen Expression, der kun er tilgængelige for underklasser og klassen selv.
+
+        abstract readonly string OperatorSymbol { } // b. Med abstrakt read-only property OperatorSymbol af typen string.
         
+        public sealed string ToString() // c. Med ToString,
+        {
+            return (left + " " + OperatorSymbol + " " + right); // der først udskriver left, så OperatorSymbol, så right (adskilt af mellemrum) og ikke kan overstyres
+        }
+
     }
 
-    sealed override Evaluate()
+    public class PlusExpression : BinaryExpression // 4. Lav en BinaryExpression underklasse PlusExpression:
     {
-        base.ToString();
-    }
-   
+        public new string OperatorSymbol // a. Med implementation af OperatorSymbol,
+        {
+            get { return "+"; } // der returnerer "+".
+        }
 
-    public abstract class BinaryExpression : Expression
+        public new double Evaluate // b. med implementation af Evaluate,
+        {
+            get { return (left+right); } // der returnerer resultat af left + right.
+        }
+    }
+
+
+    public class MinusExpression : BinaryExpression
     {
-        protected Expression left;
-        protected Expression right;
-
-        abstract readonly string OperatorSymbol;
 
     }
 
- }
+    public class MultiplyExpression : BinaryExpression
+    {
+
+    }
+
+    public class UnaryExpression : BinaryExpression
+    {
+
+    }
+    
+    public class NegateExpression : UnaryExpression
+    {
+
+    }
+
+
+}
--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Create your expression here
+            Expression expr = new PlusExpression(
+                new ConstantExpression(4),
+                new PlusExpression(
+                    new ConstantExpression(1),
+                    new ConstantExpression(2)));
+            // Example:
+            //Expression expr = new PlusExpression(
+            //    new ConstantExpression(4),
+            //    new PlusExpression(
+            //        new ConstantExpression(1),
+            //        new ConstantExpression(2)));
+
+            // We print the textual representation
+            Console.Write(expr.ToString());
+
+            Console.Write(" = ");
+
+            // ... and then the result of evaluating
+            Console.WriteLine(expr.Evaluate());
+
+            Console.ReadKey();
         }
     }
 }
